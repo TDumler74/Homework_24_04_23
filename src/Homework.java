276094b408b703ba4ddc0cefe954f85e4b487d3f
TDumler74 <tatjana.dumler@gmail.com>
2023-04-25 12:43:58 +0200

    системы счисления

---
+++ src/Homework.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Homework {
+
+  //Задача 1
+//Написать метод
+//
+//public static String decToBin(int number)
+//который получит число и вернёт строку с его двоичной записью.
+  private static final int RADIX = 2;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("Enter your number : ");
+    try {
+      int number = Integer.parseInt(br.readLine());
+      System.out.println(decToBin(number));
+    } catch (NumberFormatException e) {
+      System.out.println("Неправильный формат целого числа: " + e.getMessage());
+    }
+  }
+
+
+  public static String decToBin(int number) {
+
+    String str = " ";//инициализируем строку
+
+    number = Math.abs(number);// убираем знак
+    if (number == 0) {
+      return "0";
+    }
+    while (number != 0) { // пока в числе есть цифры
+      int digit = number % RADIX;// // последняя цифра - остаток от деления на основание (2)
+      number /= RADIX;// сам остаток от числа делим на основание (2)
+      str = digit + str;// digit-вписываем как первую цифру в нашу строку, и это всё повторяем пока number !=0.
+
+    }
+    return str;
+  }
+}
